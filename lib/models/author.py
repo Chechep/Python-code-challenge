f29f6f145945e31efa1ac847e1bb96f070e700c8
Brian Cheptiony <cheptiony6@gmail.com>
2025-05-27 14:57:55 +0300

    Test passed

--- lib/models/author.py
+++ lib/models/author.py
@@ -1,5 +1,4 @@
 from lib.db.connection import get_connection
-from lib.models.article import Article  # We'll create this file next
 
 class Author:
     def __init__(self, name, id=None):
@@ -29,17 +28,6 @@ class Author:
         return None
 
     def articles(self):
-        conn = get_connection()
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM articles WHERE author_id = ?", (self.id,))
-        rows = cursor.fetchall()
-        conn.close()
-        return [Article(id=row["id"], title=row["title"], author_id=row["author_id"], magazine_id=row["magazine_id"]) for row in rows]
-
-    def __repr__(self):
-        return f"<Author id={self.id} name='{self.name}'>"
-
-    def articles(self):
         from lib.models.article import Article  # Delayed import to avoid circular dependency
         conn = get_connection()
         cursor = conn.cursor()
@@ -61,97 +49,37 @@ class Author:
         conn.close()
         return [Magazine(id=row["id"], name=row["name"], category=row["category"]) for row in rows]
 
-# Find all authors who have written for a specific magazine
-def authors_for_magazine(magazine_id):
-    from lib.models.author import Author
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("""
-        SELECT DISTINCT au.* FROM authors au
-        JOIN articles a ON au.id = a.author_id
-        WHERE a.magazine_id = ?
-    """, (magazine_id,))
-    rows = cursor.fetchall()
-    conn.close()
-    return [Author(id=row["id"], name=row["name"]) for row in rows]
+    @classmethod
+    def for_magazine(cls, magazine_id):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT DISTINCT au.*
+            FROM authors au
+            JOIN articles a ON au.id = a.author_id
+            WHERE a.magazine_id = ?
+        """, (magazine_id,))
+        rows = cursor.fetchall()
+        conn.close()
+        return [cls(id=row["id"], name=row["name"]) for row in rows]
 
-# Find magazines with articles by at least 2 different authors
-def magazines_with_multiple_authors():
-    from lib.models.magazine import Magazine
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("""
-        SELECT m.*, COUNT(DISTINCT a.author_id) as author_count
-        FROM magazines m
-        JOIN articles a ON m.id = a.magazine_id
-        GROUP BY m.id
-        HAVING author_count >= 2
-    """)
-    rows = cursor.fetchall()
-    conn.close()
-    return [Magazine(id=row["id"], name=row["name"], category=row["category"]) for row in rows]
+    @classmethod
+    def top_author(cls):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT au.*, COUNT(a.id) AS article_count
+            FROM authors au
+            JOIN articles a ON au.id = a.author_id
+            GROUP BY au.id
+            ORDER BY article_count DESC
+            LIMIT 1
+        """)
+        row = cursor.fetchone()
+        conn.close()
+        if row:
+            return cls(id=row["id"], name=row["name"])
+        return None
 
-# Count the number of articles in each magazine
-def article_counts_per_magazine():
-    from lib.models.magazine import Magazine
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("""
-        SELECT m.*, COUNT(a.id) as article_count
-        FROM magazines m
-        LEFT JOIN articles a ON m.id = a.magazine_id
-        GROUP BY m.id
-    """)
-    rows = cursor.fetchall()
-    conn.close()
-    return [(Magazine(id=row["id"], name=row["name"], category=row["category"]), row["article_count"]) for row in rows]
-
-# Find the author who has written the most articles
-def top_author():
-    from lib.models.author import Author
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("""
-        SELECT au.*, COUNT(a.id) as article_count FROM authors au
-        JOIN articles a ON au.id = a.author_id
-        GROUP BY au.id
-        ORDER BY article_count DESC
-        LIMIT 1
-    """)
-    row = cursor.fetchone()
-    conn.close()
-    if row:
-        return Author(id=row["id"], name=row["name"])
-    return None
-@classmethod
-def for_magazine(cls, magazine_id):
-    from lib.db.connection import get_connection
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("""
-        SELECT DISTINCT au.*
-        FROM authors au
-        JOIN articles a ON au.id = a.author_id
-        WHERE a.magazine_id = ?
-    """, (magazine_id,))
-    rows = cursor.fetchall()
-    conn.close()
-    return [cls(id=row["id"], name=row["name"]) for row in rows]
-@classmethod
-def top_author(cls):
-    from lib.db.connection import get_connection
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("""
-        SELECT au.*, COUNT(a.id) AS article_count
-        FROM authors au
-        JOIN articles a ON au.id = a.author_id
-        GROUP BY au.id
-        ORDER BY article_count DESC
-        LIMIT 1
-    """)
-    row = cursor.fetchone()
-    conn.close()
-    if row:
-        return cls(id=row["id"], name=row["name"])
-    return None
+    def __repr__(self):
+        return f"<Author id={self.id} name='{self.name}'>"
