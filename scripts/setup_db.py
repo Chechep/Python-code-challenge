ab459523acfc50b243d88137a49d687afddde456
Brian Cheptiony <cheptiony6@gmail.com>
2025-05-27 10:29:11 +0300

    Database schema creation

---
+++ scripts/setup_db.py
@@ -0,0 +1,15 @@
+from lib.db.connection import get_connection
+
+def setup_db():
+    with open('lib/db/schema.sql') as f:
+        schema_sql = f.read()
+
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.executescript(schema_sql)
+    conn.commit()
+    conn.close()
+    print("Database setup complete.")
+
+if __name__ == "__main__":
+    setup_db()
